9c1dcabf540f65334e2ba9cf9c3a465f94d29b24
Kaenova Mahendra Auditama <kaenova@gmail.com>
2023-09-14 14:38:41 +0700

    Add Contact

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getContact } = require('./utils')
+const { getContact, addContact } = require('./utils')
 const app = express()
 const port = 3000
 
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
 app.get('/getContact', (req, res) => {
   const data = getContact()
   return res.json(data)
+})
+
+app.post('/addContact', express.json(), (req, res) => {
+  const name = req.body.name
+  const phoneNumber = req.body.phoneNumber
+  addContact(name, phoneNumber)
+  return res.send('Berhasil menambahkan kontak')
 })
 
 app.listen(port, () => {
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { nanoid } = require('nanoid')
 
 function getContact() {
   let jsonData = fs.readFileSync('data.json')
@@ -6,4 +7,17 @@ function getContact() {
   return data
 }
 
-module.exports = { getContact }
+function addContact(name, phoneNumber) {
+  let id = nanoid(8)
+  let jsonData = fs.readFileSync('data.json')
+  let data = JSON.parse(jsonData)
+  data.push({
+    "id": id,
+    "name": name,
+    "phone_number": phoneNumber
+  })
+  jsonData = JSON.stringify(data)
+  fs.writeFileSync('data.json', jsonData)
+}
+
+module.exports = { getContact, addContact }
