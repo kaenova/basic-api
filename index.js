297268eed25e0aaf6836b528602ffed6ea07e631
Kaenova Mahendra Auditama <kaenova@gmail.com>
2023-09-14 15:45:53 +0700

    Authentication

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { getContact, addContact, deleteContact, updateContact } = require('./utils')
 const app = express()
 const port = 3000
+const serverKeyWord = 'apa hayoo'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,6 +14,11 @@ app.get('/contact', (req, res) => {
 })
 
 app.post('/contact', express.json(), (req, res) => {
+  // Butuh penjagaan login
+  const userKeyWord = req.body.keyWord
+  if (userKeyWord !== serverKeyWord) {
+    return res.send('Anda tidak diperbolehkan menggunakan API ini')
+  }
   const name = req.body.name
   const phoneNumber = req.body.phoneNumber
   addContact(name, phoneNumber)
@@ -37,6 +43,19 @@ app.patch('/contact', express.json(), (req, res) => {
   } else {
     return res.send("Gagal mengupdate kontak")
   }
+})
+
+app.post('/login', express.json(), (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  // Sebagai contoh pengecekkan email dan password
+  // Lebih aman dengan disimpan di database serta password
+  // yang di encrypt dgn bcrypt
+  if (email === 'kaenova@bangkit.academy' && password == 'kerjago') {
+    return res.send(serverKeyWord)
+  }
+  return res.send("Anda salah password")
 })
 
 app.listen(port, () => {
